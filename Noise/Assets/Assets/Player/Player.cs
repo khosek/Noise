63c9fe60b40f7da31bf4f15b4b01ec4930d60760
khosek <khosek@iu.edu>
2023-10-24 15:06:19 -0400

    Implements basic player movement

--- Noise/Assets/Assets/Player/Player.cs
+++ Noise/Assets/Assets/Player/Player.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = Vector2.zero;
+        movement.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        movement.y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(movement);
     }
 }
