1a3de54f73f2df4d2dabacf539cf6f5072461f4a
khosek <khosek@iu.edu>
2023-10-26 16:02:23 -0400

    Implements noise decay

--- Noise/Assets/EnemySpawner.cs
+++ Noise/Assets/EnemySpawner.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] float noiseDecayRate;
+    [SerializeField] float noise;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +16,16 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (noise > 0) { noise -= noiseDecayRate * Time.deltaTime; }
     }
 
     void changeNoise(float sound) 
     {
         Debug.Log("Sound receieved: " + sound);
+        int maxNoise = (int) (sound * 100.0f) % 100;
+        int noiseChange = (int) sound;
+        if(noise < maxNoise - noiseChange) { noise += noiseChange; }
+        else if (noise < maxNoise) { noise = maxNoise; }
+        Debug.Log("Current noise: " + noise);
     }
 }
