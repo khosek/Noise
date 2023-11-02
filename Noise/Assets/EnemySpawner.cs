275ffd1701d63b6f97b294d3ee64cb1c6e3bd637
khosek <khosek@iu.edu>
2023-11-02 17:04:14 -0400

    Implements random zombie spawning

--- Noise/Assets/EnemySpawner.cs
+++ Noise/Assets/EnemySpawner.cs
@@ -6,11 +6,28 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] float noiseDecayRate;
     [SerializeField] float noise;
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] GameObject enemy;
+
+    IEnumerator SpawnZombie() {
+        while (true)
+        {
+            // Spawn in the enemy
+            GameObject newEnemy = GameObject.Instantiate(enemy);
+            int randIndex = Random.Range(0, spawnPoints.Length);
+            newEnemy.transform.position = spawnPoints[randIndex].position;
+
+            // Wait for an amount of time based on the current noise level
+            float waitTime = 2.2f - (noise / 50f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         NoiseController.instance.Sound.AddListener(changeNoise);
+        StartCoroutine(SpawnZombie());
     }
 
     // Update is called once per frame
