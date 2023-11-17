e3701115691dde3641c7aec04a98923ffb24363b
khosek <khosek@iu.edu>
2023-11-16 22:58:28 -0500

    Makes the music change dynamically

--- Noise/Assets/Assets/EnemySpawner.cs
+++ Noise/Assets/Assets/EnemySpawner.cs
@@ -12,6 +12,15 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI noiseText;
 
+    [SerializeField] AudioSource[] songs;
+    /*[SerializeField] AudioSource songLevel1;
+    [SerializeField] AudioSource songLevel2;
+    [SerializeField] AudioSource songLevel3;
+    [SerializeField] AudioSource songLevel4;*/
+    int previousSongLevel;
+    int currentSongLevel;
+    bool changingSong;
+
     IEnumerator SpawnZombie() {
         while (true)
         {
@@ -25,12 +34,28 @@ public class EnemySpawner : MonoBehaviour
             yield return new WaitForSeconds(waitTime);
         }
     }
+
+    IEnumerator changeVolumeLevel(AudioSource song, float start, float end)
+    {
+        float timeElapsed = 0.0f;
+        while (timeElapsed < 2)
+        {
+            song.volume = Mathf.Lerp(start, end, timeElapsed);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        song.volume = end;
+        changingSong = false;
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         NoiseController.instance.Sound.AddListener(changeNoise);
         StartCoroutine(SpawnZombie());
+        currentSongLevel = 0;
+        previousSongLevel = currentSongLevel;
+        changingSong = false;
     }
 
     // Update is called once per frame
@@ -38,6 +63,24 @@ public class EnemySpawner : MonoBehaviour
     {
         if (noise > 0) { noise -= noiseDecayRate * Time.deltaTime; }
         noiseText.text = "Noise: " + (int) noise;
+
+        /*songLevel1.volume = 1.0f - (noise / 25.0f);
+        songLevel2.volume = getMin((noise - 25.0f) / 25.0f, 1.0f - ((noise - 25.0f) / 25.0f));
+        songLevel3.volume = getMin((noise - 50.0f) / 25.0f, 1.0f - ((noise - 50.0f) / 25.0f));
+        songLevel4.volume = (noise - 75.0f) / 25.0f;*/
+
+        // Determining what song level should be playing
+        if ((noise > (currentSongLevel + 1) * 25 || noise < currentSongLevel * 25) && !changingSong)
+        {
+            previousSongLevel = currentSongLevel;
+            currentSongLevel = (int) (noise / 25.0f); 
+            Debug.Log("Previous song level is " + previousSongLevel);
+            Debug.Log("New song level is " + currentSongLevel);
+
+            StartCoroutine(changeVolumeLevel(songs[previousSongLevel], 1.0f, 0.0f));
+            StartCoroutine(changeVolumeLevel(songs[currentSongLevel], 0.0f, 1.0f));
+            changingSong = true;
+        }
     }
 
     void changeNoise(float sound) 
